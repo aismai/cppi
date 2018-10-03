1050ef3168497832b63f530ea1f01d17165fda92
aismai <abai.ismailov@gmail.com>
2018-10-03 15:34:30 +0600

    install create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>CPPI APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
