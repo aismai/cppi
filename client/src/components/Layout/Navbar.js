4933f6604399a0d218f764b3420fef6ede9cf45b
aismai <abai.ismailov@gmail.com>
2018-10-07 17:53:00 +0600

    refactor navbar

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,30 +1,48 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { Layout, Menu } from "antd";
-
-const { Header } = Layout;
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
   render() {
+    const { isAuthenticated, user } = this.props.auth;
     return (
-      <Header style={{ background: "#fff", borderBottom: "1px solid #e8e8e8" }}>
-        <div className="logo">
-          <img src="/img/logo.svg" alt="logo" />
-        </div>
-        <Menu mode="horizontal" style={{ lineHeight: "63px" }}>
-          <Menu.Item key="1">
-            <Link to="/">home</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/login">login</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/register">register</Link>
-          </Menu.Item>
-        </Menu>
-      </Header>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/" className="logo">
+              Home
+            </Link>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li className="nav-item__right">
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navbar);
