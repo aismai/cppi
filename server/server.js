ac57b7d3e85066412950282466674d3640d94047
aismai <abai.ismailov@gmail.com>
2018-11-28 10:55:52 +0600

    router bug fix

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ app.use(cookieParser());
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 // DB config
 mongoose.Promise = global.Promise;
 mongoose
