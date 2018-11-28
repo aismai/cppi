8f7e28d50675d590820f7192a3461fc7e842403c
aismai <abai.ismailov@gmail.com>
2018-11-28 10:01:09 +0600

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
+const path = require("path");
 
 require("dotenv").config();
 const users = require("./routes/api/users");
@@ -22,6 +23,15 @@ require("./config/passport")(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // DB config
 mongoose.Promise = global.Promise;
