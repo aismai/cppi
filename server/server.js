626792d15bd8b64249cc7f8d1ceb1c9386289d7f
aismai <abai.ismailov@gmail.com>
2018-11-28 11:44:23 +0600

    production routes fix

--- server/server.js
+++ server/server.js
@@ -41,18 +41,14 @@ app.use("/api/questions", questions);
 app.use("/api/personal-data", personalData);
 app.use("/api/surveys", surveys);
 
+console.log(path.resolve(__dirname, "client", "build", "index.html"));
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    let url = res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html")
-    );
-
-    if (!url.startsWith("/app/")) url = url.substring(1);
-    res.sendFile(url);
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
